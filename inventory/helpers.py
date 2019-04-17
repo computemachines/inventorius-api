e05a79baf26fdaee4e31bed4fb0bed791462df68
Tyler Parker <tparker@computemachines.com>
2019-04-17 10:31:46 -0400

    Add a json encoder for my helper resource classes.

--- inventory/helpers.py
+++ inventory/helpers.py
@@ -1,8 +1,16 @@
+import json
 
 class Bin:
     def __init__(self, json=None):
         self._id = json['id']
         self.props = json['props']
         
-    def __repr__(self):
+    def __str__(self):
         return "Bin(id={}, props={})".format(self._id, self.props)
+
+    def toJson(self):
+        return '{{"id": "{}", "props": {}}}'.format(self._id, self.props)
+
+class MyEncoder(json.JSONEncoder):
+    def default(self, o):
+        return {'id': o._id, 'props': o.props}
