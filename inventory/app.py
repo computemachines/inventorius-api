ab1007e7dc138d26c3ba5cf5cb05a9cbe1f8c98c
Tyler Parker <tparker@computemachines.com>
2020-06-15 13:49:14 -0400

    Fix problem with multiple docs in admin with same type

--- inventory/app.py
+++ inventory/app.py
@@ -62,17 +62,17 @@ def admin_increment_code(prefix, code):
     if code_number >= next_unused:
         max_code = code_number
         if prefix == "SKU":
-            db.admin.insert_one({"type": "SKU",
-                                 "next": f"SKU{max_code+1:06}"})
+            db.admin.replace_one({"_id": "SKU"}, {"_id": "SKU",
+                                                  "next": f"SKU{max_code+1:06}"})
         if prefix == "UNIQ":
-            db.admin.insert_one({"type": "UNIQ",
-                                 "next": f"UNIQ{max_code+1:05}"})
+            db.admin.replace_one({"_id": "UNIQ"}, {"_id": "UNIQ",
+                                                   "next": f"UNIQ{max_code+1:05}"})
         if prefix == "BATCH":
-            db.admin.insert_one({"type": "BATCH",
-                                 "next": f"BATCH{max_code+1:04}"})
+            db.admin.replace_one({"_id": "BATCH"}, {"_id": "BATCH",
+                                                    "next": f"BATCH{max_code+1:04}"})
         if prefix == "BIN":
-            db.admin.insert_one({"type": "BIN",
-                                 "next": f"BIN{max_code+1:06}"})
+            db.admin.replace_one({"_id": "BIN"}, {"_id": "BIN",
+                                                  "next": f"BIN{max_code+1:06}"})
 
 
 def admin_get_next(prefix):
@@ -86,25 +86,25 @@ def admin_get_next(prefix):
                 max_value = code_number
         return max_value
 
-    next_code_doc = db.admin.find_one({"type": prefix})
+    next_code_doc = db.admin.find_one({"_id": prefix})
     if not next_code_doc:
         if prefix == "SKU":
             max_value = max_code_value(db.sku, "SKU")
-            db.admin.insert_one({"type": "SKU",
+            db.admin.insert_one({"_id": "SKU",
                                  "next": f"SKU{max_value+1:06}"})
         if prefix == "UNIQ":
             max_value = max_code_value(db.uniq, "UNIQ")
-            db.admin.insert_one({"type": "UNIQ",
+            db.admin.insert_one({"_id": "UNIQ",
                                  "next": f"UNIQ{max_value+1:05}"})
         if prefix == "BATCH":
             max_value = max_code_value(db.batch, "BATCH")
-            db.admin.insert_one({"type": "BATCH",
+            db.admin.insert_one({"_id": "BATCH",
                                  "next": f"BATCH{max_value+1:04}"})
         if prefix == "BIN":
             max_value = max_code_value(db.bin, "BIN")
-            db.admin.insert_one({"type": "BIN",
+            db.admin.insert_one({"_id": "BIN",
                                  "next": f"BIN{max_value+1:06}"})
-        next_code_doc = db.admin.find_one({"type": prefix})
+        next_code_doc = db.admin.find_one({"_id": prefix})
     return next_code_doc['next']
 
 
