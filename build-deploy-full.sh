4e38d80c4e21f01b40b122cf90c50f18fc1ba2d6
Tyler Parker <tparker@computemachines.com>
2021-06-28 18:41:55 -0400

    ci(github): Merge deploy workflow into build-release workflow.
    fix(deploy): Fix ssl cert location.
    
    ci(github): Update ssh keys for deployment.

--- build-deploy-full.sh
+++ build-deploy-full.sh
@@ -5,6 +5,7 @@ EMAIL=tparker@computemachines.com
 
 cd /root
 rm -r inventory # fresh install
+rm inventory-frontend-package.tar.gz
 
 echo "---- Checking user -------"
 if ! id www-uwsgi-inventory-api &>/dev/null
@@ -73,7 +74,7 @@ fi
 # rm -rf temp-build/
 
 echo "---- Downloading Frontend Release ----"
-wget https://github.com/computemachines/inventory-frontend/releases/latest/inventory-frontend-package.tar.gz
+wget https://github.com/computemachines/inventory-frontend/releases/latest/download/inventory-frontend-package.tar.gz
 
 echo "---- Installing -----------"
 
@@ -83,34 +84,32 @@ certbot certonly -n -m $EMAIL --agree-tos --standalone $DOMAINS
 
 echo "---- Installing Frontend --"
 mkdir -pv inventory/node-deployment
-pushd inventory
+cd /root/inventory/
 tar -xzf ../inventory-frontend-package.tar.gz -C node-deployment
-pushd node-deployment
+cd /root/inventory/node-deployment/
 npm ci --production
 cp -v inventory-frontend.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl enable inventory-frontend
-popd
 
 echo "---- Installing NGINX conf ----"
-pushd node-deployment
-cp -v inventory-nginx.conf /
-popd
+cd /root/inventory/node-deployment/
+cp -v inventory-nginx.conf /etc/nginx/sites-available/
+rm /etc/nginx/sites-enabled/default -v
+ln -sv /etc/nginx/sites-available/inventory-nginx.conf /etc/nginx/sites-enabled/inventory-nginx.conf
 
 echo "---- Installing Backend ----"
+cd /root/inventory/
 git clone https://github.com/computemachines/inventory-api.git
-pushd inventory-api
-git pull
-chown -Rv www-data:www-data ../
-chown -Rv www-uwsgi-inventory-api:www-data .
+cd /root/inventory/inventory-api/
+chown -R www-data:www-data ../
+chown -R www-uwsgi-inventory-api:www-data .
 cp inventory-api.{service,socket} /etc/systemd/system/
 systemctl daemon-reload
 systemctl enable inventory-api.service
 systemctl enable inventory-api.socket
 pip install -r requirements.txt
-popd
 
-popd
 echo "---- Starting --------------"
 systemctl start inventory-frontend
 systemctl start mongod
