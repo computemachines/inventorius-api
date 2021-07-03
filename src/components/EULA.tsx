578808897559834cea47cf9d99c87b7b3cae3a55
Tyler Parker <tparker@computemachines.com>
2021-07-03 16:37:42 -0400

    typo

--- src/components/EULA.tsx
+++ src/components/EULA.tsx
@@ -7,7 +7,7 @@ function EULA() {
     <>
       <h2>Attribution</h2>
       <code>
-        All svg icons with css classname starting with "lnr-" is licensed by
+        All svg icons with css classname starting with "lnr-" are licensed by
         Roonas (https://linearicons.com) under CC BY-SA 4.0 license.
       </code>
     </>
